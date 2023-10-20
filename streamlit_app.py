d558a198e9dc4e27b00593679d863e770d36f4fe
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2023-10-20 00:22:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.write('Hello, *World!* :sunglasses:')
 
 st.write('Qual é o seu nome?')
-nome = st.text_input()
+nome = st.text_input('Nome','')
 st.write('Olá, ', nome, '!!')
